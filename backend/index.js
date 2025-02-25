17c8f86d6bdb730efb986fbf78bec0f0f5d0f133
Lobna Mansouri <lobnamansouri91@gmail.com>
2025-02-25 16:00:05 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,7 @@ app.get('*' , (req , res)=>{
 })
 
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname + "/uploads"))); 
+app.use("/uploads", express.static(path.join(_dirname + "../uploads"))); 
 
 app.get("/", (req, res) => {
   res.send("API is running...");
